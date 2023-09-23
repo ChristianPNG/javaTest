66e74eb6f1849e8a37f18340747e619828233f14
ChristianPNG <rchris0214@gmail.com>
2023-09-22 23:54:40 -0400

    tic tac toe

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,10 +1,58 @@
 package Game;
 
+import java.util.Scanner;
+
 public class Game {
-
+	protected static Scanner scanner = new Scanner(System.in);
+	protected static int[][] GameState = new int[9][2];
+	
 	public static void main(String[] args) {
-		Interface Display = new Interface();
-		Display.display();
+		Interface UI = new Interface();
+		Game MainGame = new Game();
+		int coin = 0;
+		while(MainGame.WinCondition() == false) {
+			UI.display();
+			UI.prompt(coin);
+			if (coin == 0) {
+				coin = 1;
+			}
+			else {
+				coin = 0;
+			}
+		}
+		UI.display();
+		System.out.print("\n winner");
+		scanner.close();
 	}
-
+	
+	public boolean WinCondition() {
+		for (int i = 0; i<9; i++) {
+			if (GameState[i][1] != 0 && (HorizontalCheck(i) || VerticalCheck(i) || DiagonalCheck(i))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean HorizontalCheck(int i) {
+		if ((i == 0 || i == 3 || i == 6) && GameState[i][1] == GameState[i+1][1] && GameState[i][1] == GameState[i+2][1]) {
+			return true;
+		}
+		return false;
+	}
+	public boolean VerticalCheck(int i) {
+		if ((i == 0 || i == 1 || i==2) && GameState[i][1] == GameState[i+3][1] && GameState[i][1] == GameState[i+6][1] ) {
+			return true;
+		}
+		return false;
+	}
+	public boolean DiagonalCheck(int i) {
+		if (i == 0 && GameState[i][1] == GameState[i+4][1] && GameState[i][1] == GameState[i+8][1]) {
+			return true;
+		}
+		else if (i == 2 && GameState[i][1] == GameState[i+2][1] && GameState[i][1] == GameState[i+4][1]) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/main/java/Game/Interface.java
+++ src/main/java/Game/Interface.java
@@ -1,20 +1,15 @@
 package Game;
 
-import java.util.Scanner;
-
-public class Interface {
+public class Interface extends Game{
 	public void display() {
-		
-		int[][] GameState = new int[9][2];
-		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("\n");
 		for (int i = 0; i<9; i++) {
 			if (i % 3 == 0 && i != 0) {
 				System.out.print("\n-+-+-\n");
 			}
-			if (GameState[i][0] == 1) {
-				if(GameState[i][1] == 0) {
+			if (GameState[i][0] == 1) { //[][0] == SPOT TAKEN?
+				if(GameState[i][1] == 2) { //[][1] == THE SHAPE
 					System.out.print("O");
 				}
 				else {
@@ -28,21 +23,37 @@ public class Interface {
 				System.out.print("|");
 			}
 		}
+		
+	}
+	
+	public void prompt(int coin) {
 		System.out.print("\nEnter your placement (1-9): ");
 		while(true) {
+			//if scanner does not have nextInt. give an error
+			//this is looped as its always waiting until the correct response is given
 			while(!scanner.hasNextInt()) {
 				scanner.next();
 				System.out.print("\n Wrong input, try again");
 			}
+			//at this point we do have an int as our input
 			int num = scanner.nextInt();
-			if (num > -1 && num <= 9) {
-				System.out.print("\n Correct input");
+			if (num > 0 && num <= 9) {
+				if (GameState[num - 1][0] == 1) {
+					System.out.print("\n Spot is taken, try again");
+					continue;
+				}
+				GameState[num - 1][0] = 1;
+				if (coin == 1) {
+					GameState[num - 1][1] = 2;
+				}
+				else {
+					GameState[num - 1][1] = 1;
+				}
 				break;
 			}
 			else {
 				System.out.print("\n Wrong input, try again");
 			}
 		}
-		scanner.close();
 	}
 }
